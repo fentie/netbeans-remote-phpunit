24455514586f16d01af748b2e0ff31689f3b2749
Brian Fenton <fentie@gmail.com>
2013-06-23 01:29:17 -0700

    initial commit of script

---
+++ phpunit-remote.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+#echo $@ #uncomment to debug
+
+REMOTE_SERVER=user@virtualhost
+LOCAL_PATH_TO_TESTS='/path/to/tests'
+REMOTE_PATH_TO_TESTS='/path/to/tests'
+
+# The very last argument is the path to the test(s) you want run
+export TEST=$9
+
+# Chop off everything up to "tests" in that path. "tests" is just an example path
+TEST=${TEST##r*tests}
+
+# Connect to your VM, cd to your test location and run phpunit with most of the args
+ssh $REMOTE_SERVER "cd $PATH_TO_TESTS; phpunit $1 $2 $3 $4 $REMOTE_PATH_TO_TESTS/$TEST"
+
+# Copy the test output back to your local machine, where NetBeans expects to find it
+scp $REMOTE_SERVER:$2 $2
